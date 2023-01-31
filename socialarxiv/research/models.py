3d96ab104c0593c58e5fd450f930331a07036863
Job Hernandez <hj93@protonmail.com>
2023-01-30 16:03:40 -0800

    add basis for following system.

--- socialarxiv/research/models.py
+++ socialarxiv/research/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from django.contrib.auth import get_user_model
 
 
 class Paper(models.Model):
@@ -41,3 +42,24 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.name} on {self.paper}'
+
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+user_model = get_user_model()
+user_model.add_to_class('following',
+                        models.ManyToManyField('self',
+                            through=Contact,
+                            related_name='followers',
+                            symmetrical=False))
